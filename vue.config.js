84c8704cc002c6757a0c4278851ef548bb3a2c51
ibynx <36119601+ibynx@users.noreply.github.com>
2021-01-01 09:42:18 -0500

    vue injection hopefully

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,17 @@
 module.exports = {
     publicPath: '/greenpaint/',
-    runtimeCompiler: true
+    runtimeCompiler: true,
+    pages: {
+        index: {
+            template: 'public/index.html'
+        },
+        demo: {
+            entry: 'src/main.js',
+            template: 'public/demo_puzzle.html'
+        },
+        custom: {
+            entry: 'src/main.js',
+            template: 'public/custom_puzzle.html'
+        }
+    }
 }
